da00a8af2048c9a9a697d843feaa19a68e2ee70d
Eugene Kpakpo Acquaye <eugeneacquaye.ea6@gmail.com>
2025-01-12 23:34:54 +0000

    create an introductory page

---
+++ src/pages/Intro.jsx
@@ -0,0 +1,21 @@
+import { useNavigate } from 'react-router'
+import puzzled from '../assets/puzzled.svg'
+import { Button } from '@mui/material'
+import Footer from '../components/Footer'
+
+const Intro = () => {
+    const navigate = useNavigate()
+
+  return (
+    <>    <div className='flex justify-around flex-col items-center m-5 relative'>
+        <img src={puzzled} alt="" className='px-3 mb-2' />
+        <div className=' flex flex-wrap justify-center'><h1 className='w-full text-2xl font-medium  my-1 text-center p-6 '>Feeling confident? <br />Test your knowledge on BuzzHive's Trivia quizzes</h1></div>
+        <Button onClick={() => navigate('/home')} variant="contained" size = 'medium' style={{ fontWeight:'bolder', cursor: 'pointer', position:'relative', bottom: '-15px'}}>Get Started</Button>
+    </div>
+        <Footer />
+    </>
+
+  )
+}
+
+export default Intro
