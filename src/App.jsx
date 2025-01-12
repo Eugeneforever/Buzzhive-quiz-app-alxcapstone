0c7dfb19a54f6699144a7b6171ebb3f72e4b16fe
Eugene Kpakpo Acquaye <eugeneacquaye.ea6@gmail.com>
2025-01-12 23:36:54 +0000

    render all components to the react dom

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+// import { useState } from 'react'
+
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Routes, Route, BrowserRouter } from "react-router";
+import Home from "./pages/Home";
+import QuizPage from "./pages/QuizPage";
+import Result from "./pages/Result";
+import Intro from "./pages/Intro";
+import { useState } from "react";
+import axios from "axios";
+
+function App() {
+	const [user, setUser] = useState("");
+	const [questions, setQuestions] = useState();
+	const [score, setScore] = useState(0);
+
+	const fetchQuestions = async (category = "", difficulty = "") => {
+		const { data } = await axios.get(
+			`https://opentdb.com/api.php?amount=10${
+				category && `&category=${category}`
+			}${difficulty && `&difficulty=${difficulty}`}&type=multiple`
+		);
+		setQuestions(data.results);
+    
+	};
+
+	return (
+		<BrowserRouter>
+			<div className="font-montserrat">
+				<Header />
+          
+				<Routes>
+					<Route path="/" element={<Intro />} />
+					<Route
+						path="/home"
+						element={
+              <Home
+								user={user}
+								setUser={setUser}
+								fetchQuestions={fetchQuestions}
+							/>
+						}
+					/>
+					<Route
+						path="/quiz"
+						element={
+							<QuizPage
+								user={user}
+								questions={questions}
+								setQuestions={setQuestions}
+								score={score}
+								setScore={setScore}
+							/>
+						}
+					/>
+					<Route path="/result" element={<Result score={score} user={user}/>} />
+				</Routes>
+
+			</div>
+		</BrowserRouter>
+	);
+}
+
+export default App;
