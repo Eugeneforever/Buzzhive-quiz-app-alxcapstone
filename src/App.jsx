541429c7b8be627cbb78c9d8f975e17230c0999a
Eugene Kpakpo Acquaye <eugeneacquaye.ea6@gmail.com>
2025-01-13 10:07:50 +0000

    delete the app.css import from to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-// import { useState } from 'react'
 
-import "./App.css";
+
 import Header from "./components/Header";
 import { Routes, Route, BrowserRouter } from "react-router";
 import Home from "./pages/Home";
