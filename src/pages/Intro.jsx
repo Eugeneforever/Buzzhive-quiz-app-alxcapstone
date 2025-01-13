1795e6dd13f6aa012dd3ac6e0661544abadf36b0
Eugene Kpakpo Acquaye <eugeneacquaye.ea6@gmail.com>
2025-01-13 10:07:02 +0000

    fix media responsive breakpoint on get started button on small devices

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -10,7 +10,7 @@ const Intro = () => {
     <>    <div className='flex justify-around flex-col items-center m-5 relative'>
         <img src={puzzled} alt="" className='px-3 mb-2' />
         <div className=' flex flex-wrap justify-center'><h1 className='w-full text-2xl font-medium  my-1 text-center p-6 '>Feeling confident? <br />Test your knowledge on BuzzHive's Trivia quizzes</h1></div>
-        <Button onClick={() => navigate('/home')} variant="contained" size = 'medium' style={{ fontWeight:'bolder', cursor: 'pointer', position:'relative', bottom: '-15px'}}>Get Started</Button>
+        <Button onClick={() => navigate('/home')} variant="contained" size = 'medium' style={{ fontWeight:'bolder', cursor: 'pointer', position:'relative', bottom: '10px'}}>Get Started</Button>
     </div>
         <Footer />
     </>
