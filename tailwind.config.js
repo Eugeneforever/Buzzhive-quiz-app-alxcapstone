2bff2c69b0836d20b6c95e1c890a0f8931d7c83d
Eugene Kpakpo Acquaye <eugeneacquaye.ea6@gmail.com>
2025-01-16 16:58:35 +0000

    Add custom media screen breakpoints to tailwind.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,18 @@ export default {
       fontFamily: {
         montserrat: ['Montserrat', 'poppins', 'Roboto'],
     },
+    screens: {
+      'md-': '741px',
+      // => @media (min-width: 741px) { ... }
+      'beforesm': '386px',
+      // => @media (min-width: 386px) { ... }
+      'md+': '808px',
+      // => @media (min-width: 808px) { ... }
+      'lg+': '1164px',
+      // => @media (min-width: 1164px) { ... }
+      
+    },
+    
   },
   plugins: [],
 }
